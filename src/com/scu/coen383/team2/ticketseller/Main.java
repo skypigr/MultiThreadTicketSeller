7cab831e1abfb09277369228ce9a0905091f625a
xzhu <skypigr@gmail.com>
2019-02-04 14:24:43 -0800

    init project

---
+++ src/com/scu/coen383/team2/ticketseller/Main.java
@@ -0,0 +1,9 @@
+package com.scu.coen383.team2.ticketseller;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        return;
+    }
+
+}
